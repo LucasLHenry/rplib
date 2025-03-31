8deaefc081dd4297625e00fee71c6819e085d317
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2025-03-30 17:11:17 -0700

    refactor to make paths easier

--- utils/utils.h
+++ utils/utils.h
@@ -27,7 +27,7 @@ inline int32_t rand_i32() {
     return static_cast<int32_t>(rand_u32() - (1 << 31));
 }
 
-
+// have to do it this way because of weird linker things
 template <typename T>
 inline T xfade(T a, T b, float blend);
 #include "utils_impl.h"
@@ -65,6 +65,40 @@ inline float cos2pif(float x) {
 
 inline float sign(float x) {
     return (x > 0)? 1.0f : -1.0f;
+}
+
+inline float soft_limit(float x) {
+    if (x >= 3.0) return 1.0;
+    if (x <= -3.0) return -1.0;
+    return x * (27.0f + x * x) / (27.0f + 9.0f * x * x);
+}
+
+#define ONE_POLE_LPF(out, in, coefficient) out += (in - out) * coefficient
+
+// normalized frequency is freq (Hz) / sample rate (Hz)
+inline float ewma_filter_coefficient(float normalized_freq) {
+    return normalized_freq / (1.0f + normalized_freq);
+}
+
+// from Emilie Gillet
+inline float this_blep(float t) {
+    return 0.5f * t * t;
+}
+
+inline float next_blep(float t) {
+    t = 1.0f - t;
+    return -0.5f * t * t;
+}
+
+inline float next_iblep(float t) {
+    const float t1 = 0.5f * t;
+    const float t2 = t1 * t1;
+    const float t4 = t2 * t2;
+    return 0.1875f - t1 + 1.5f * t2 - t4;
+}
+
+inline float this_iblep(float t) {
+    return next_iblep(1.0f - t);
 }
 
 #endif  // UTILS_H_
